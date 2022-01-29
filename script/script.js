7c6347f7af1a6226c801cc27ff711fa9ece7859a
Dipendra <x42v94.ds@gmail.com>
2022-01-29 15:11:50 +0000

    minor changes

--- script/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByClassName('Tab_1').onclick = function changeContent() {
-
-    document.getElementsByClassName('Tab_1').textContent = "GO HOME";
-    document.getElementsByClassName('Tab_1').style = "Color: red";
- 
- }
